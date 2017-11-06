65eca27a831f2903b18ed90c8b8c87b2fe3311a6
garbanzos <yiyant@gmail.com>
2017-11-06 23:27:08 +0800

    done ex2 code

---
+++ L10-ok.c
@@ -0,0 +1,39 @@
+/*
+Name: Tan Yi Yan
+Matric. no.: A0127051U
+Email: yiyan@u.nus.edu
+CS4239 Lab 10 Exercise 2
+*/
+
+#include <stdio.h>
+#include <stddef.h>
+
+int offset = 0;
+char *stack;
+
+void zero_buf(volatile char *p, size_t n)
+{
+	int i;
+	for (i=0; i < n; i++) p[i] = 0;
+}
+
+void hide(void)
+{
+	char secret[] = "12345", *p;
+	int a;
+	printf("&secret %p &a %p\n", secret, &a);
+	printf("secret = %s\n", secret);
+	p = (char *) &a;
+	offset = secret - p;
+	stack = p;
+	zero_buf(secret, 5);
+}
+
+int main()
+{
+	hide();
+	printf("peek @%p: %d %d %d %d %d\n", stack+offset,
+		stack[offset], stack[offset+1], stack[offset+2],
+		stack[offset+3], stack[offset+4]);
+	return 0;
+}
